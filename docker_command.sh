684176b0767c845f0ba5166c086dab2eee97b124
Marc O'Morain <marc@circleci.com>
2021-08-26 13:45:12 +0100

    Add a class for env vars to prevent wrapping
    The table of environment variable was wrapping them. This change adds a
    new CSS class for environment variables that can be used to prevent
    wrapping.
    
    I also needed to fix the docker-compose setup to get it working locally.

---
+++ docker_command.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -o errexit
+set -o pipefail
+set -o xtrace
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+source $NVM_DIR/nvm.sh
+
+nvm install 14.1
+npm install
+npm run webpack-prod
+
+apt-get update -y
+apt-get install -y cmake pkg-config
+
+bundle install
+bundle exec jekyll clean
+bundle exec jekyll serve -s jekyll --incremental --host=0.0.0.0 --trace
